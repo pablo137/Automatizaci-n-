bc590efab0cd67c56bea8a418393b4c3f31ef8b3
fabi-cm <dahofcmmejia@gmail.com>
2023-11-25 19:27:36 -0400

    Support TC: The support a back to home

--- features/step_definitions/support.step.rb
+++ features/step_definitions/support.step.rb
@@ -1,14 +1,10 @@
-Given(/^I am on the Mercury Tours homepage$/) do
-  page.driver.browser.manage.window.maximize
-  visit('https://demo.guru99.com/test/newtours/index.php')
-
-end
-
-Given(/^I click the "([^"]*)" link$/) do |linkText|
-  click_link(linkText)
-  # sleep 10
-end
-
 Then(/^A web site under construction message$/) do
-  expect(page).to have_content("This section of our web site is currently under construction. ")
+  expected_message = "This section of our web site is currently under construction."
+  expect(page).to have_content(expected_message)
+end
+
+When(/^I press the BACK TO HOME button$/) do
+  xpath = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[4]/td/a'
+  find(:xpath, xpath).click
+  sleep 2
 end
--- features/support.feature
+++ features/support.feature
@@ -6,3 +6,10 @@ Scenario: Enter the support section
     Given I am on the Mercury Tours homepage
     And I click the "SUPPORT" link
     Then A web site under construction message
+
+@supportToHome
+Scenario: The support a back to home
+    Given I am on the Mercury Tours homepage
+    And I click the "SUPPORT" link
+    When I press the BACK TO HOME button
+    Then I am on the Mercury Tours homepage
