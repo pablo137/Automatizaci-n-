97f9219189635984512d5b5f82669be2a33e939f
pablo137 <p48102611@gmail.com>
2023-11-27 11:52:43 -0400

    sign-on TC Validate an unregistered user

--- features/sign-on.feature
+++ features/sign-on.feature
@@ -1,14 +1,25 @@
-Feature: Guru99 Demo Page
+Feature: Login
   As a BDD student
   I want to test Guru99 login page
   so I learn how to use Capybara
   
 Background:
-  Given I am on the login page
+  Given I am on the main page
+  And Enter on the login page
 
-
+@SmokeTest
 @maximize
-Scenario: Validate a successfully authenticated user       
-  Given I enter my user name and password valid
+Scenario: Validate a successfully authenticated user
+  Given I enter the following user credentials:
+    | username | pepe |
+    | password | 123  |       
   When I press the button "submit"
   Then A "Login Successfully" message is then displayed on the screen
+
+@maximize
+Scenario: Validate an unregistered user
+  Given I enter the following user credentials:
+    | username | fakeuser |
+    | password | 123  |       
+  When I press the button "submit"
+  Then A "Enter your userName and password correct" message is then displayed on the screen
--- features/step_definitions/sign-on.step.rb
+++ features/step_definitions/sign-on.step.rb
@@ -1,16 +1,26 @@
-Given('I am on the login page') do
+Given('I am on the main page') do
     visit 'https://demo.guru99.com/test/newtours/index.php'
-    xpath_base = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a'
-    element = find(:xpath, xpath_base)
-    url = element[:href].split('#').first
-    visit(url)
+end
 
-  end
+Given('Enter on the login page') do
+  xpath_base = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a'
+  element = find(:xpath, xpath_base)
+  url = element[:href].split('#').first
+  visit(url)
+end
 
-Given('I enter my user name and password valid') do
-    fill_in 'userName', :with => 'pepe123'
-    fill_in 'password', :with => '123'
-  end
+# Given('I enter the following user credentials') do |string|
+#   fill_in 'userName', :with => string
+# end
+# Given('I enter Password {string}') do |string|
+#   fill_in 'password', :with => string
+# end
+
+Given("I enter the following user credentials:") do |table|
+  credentials = table.rows_hash
+  fill_in 'userName', with: credentials['username']
+  fill_in 'password', with: credentials['password']
+end
 
 When('I press the button {string}') do |name_button|
   button = find_button(name_button)
@@ -20,3 +30,5 @@ end
 Then('A {string} message is then displayed on the screen') do |msg|
     expect(page).to have_content(msg)
 end
+
+
