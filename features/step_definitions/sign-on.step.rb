57f4581323a6b20cf67bb16d29d0bf020a99c89c
pablo137 <p48102611@gmail.com>
2023-12-07 15:43:36 -0400

    Add exception raise sign-on

--- features/step_definitions/sign-on.step.rb
+++ features/step_definitions/sign-on.step.rb
@@ -9,11 +9,17 @@ Given('I enter {string} with {string} and verify {float} characters') do |field,
   expect(value.length).to eq(verify)
 end
 
-When('I remark the field {string}') do |string|
-  input_value = find_field(string).value
+When('I remark the field {string}') do |field|
+  input_value = find_field(field).value
 end
 
-Then('the field {string} should have only {float} characters.') do |field, float|
+Then('the field {string} should have only {float} characters.') do |field, expected|
   input_value = find_field(field).value
-  expect(input_value.length).to eq(float)
+  actual_length = input_value.length
+
+  if actual_length != expected
+    raise "it should display an alert message that says: Only a maximum of #{actual_length} characters are allowed"
+  else
+    expect(actual_length).to eq(expected)
+  end
 end
