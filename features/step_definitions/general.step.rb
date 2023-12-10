335f12db86ee41552ac0d3913177d4e6e8171b02
fabi-cm <dahofcmmejia@gmail.com>
2023-12-10 19:13:10 -0400

    Creacion de un nuevo caso general en general.step.rb : I should be back on the Mercury Tours homepage

--- features/step_definitions/general.step.rb
+++ features/step_definitions/general.step.rb
@@ -11,6 +11,11 @@ Then('A {string} message is then displayed on the screen') do |msg|
     expect(page).to have_content(msg)
 end
 
-Given(/^I am on the Mercury Tours homepage$/) do
+Given("I am on the Mercury Tours homepage") do
     visit('https://demo.guru99.com/test/newtours/index.php')
+end
+
+Then("I should be back on the Mercury Tours homepage") do
+    homepage_url = 'https://demo.guru99.com/test/newtours/index.php'
+    expect(page).to have_current_path(homepage_url, ignore_query: true)
 end
